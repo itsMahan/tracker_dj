55de722fe99762b0ecadf296fd197791f4b9a690
itsMahan <ataeifarmahan@gmail.com>
2025-08-02 16:33:55 -0400

    add show all page

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path('list', views.list_trackers),
-    path('add', views.add_tracker),
+    path('list', views.list_trackers, name='list_trackers'),
+    path('add', views.add_tracker, name='add_tracker'),
     path('delete/<int:id>', views.delete_tracker, name='delete_tracker'),
 ]
